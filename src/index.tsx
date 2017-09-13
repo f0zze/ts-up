5f9ff98efbaf129c5283e188ee603b652093db0b
Aigars.Beinarovichs <a.beinarovics@gmail.com>
2017-09-13 10:27:23 +0300

    process env fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import RedBox from 'redbox-react';
+
 import Main from './Main';
 
 import './emotion/fonts';
 import './emotion/globals';
 
-ReactDOM.render(<Main />, document.getElementById('app'));
+const root = document.getElementById('app');
+
+if (process.env.NODE_ENV === 'development') {
+    try {
+        ReactDOM.render(<Main/>, root);
+    } catch (e) {
+        ReactDOM.render(<RedBox error={e}/>, root);
+    }
+} else {
+    ReactDOM.render(<Main/>, root);
+}
+
