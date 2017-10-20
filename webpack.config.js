361cc671266055a19117feb15cc26a9e458200b4
Aigars.Beinarovichs <a.beinarovics@gmail.com>
2017-10-20 12:32:15 +0300

    remove loader

--- webpack.config.js
+++ webpack.config.js
@@ -67,8 +67,7 @@ module.exports = env => {
                 { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
                 { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
                 { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader' },
-                { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' },
-                { test: /\.(png|jpg|jpeg|gif)$/, use: 'file-loader' }
+                { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' }
             ]
         },
         plugins: removeEmpty([
